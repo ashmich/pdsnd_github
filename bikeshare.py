9b6f6a6fc75c44d0881a8b03e8cb487990dbd077
AshleyTang <ashleymich089@gmail.com>
2021-06-05 23:06:58 +0800

    add comments explaining lower method

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    
+    # use .lower() to ensure that user is able to input characters whether uppercase or lowercase 
     valid_cities = ('chicago','new york city', 'washington')
     wrong_city = True
     while wrong_city:
